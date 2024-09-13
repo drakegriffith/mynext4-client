e033226508e19e9f7d1e055c0793f4ad6316a50e
Drake Griffith <Drakegriffith8@gmail.com>
2024-09-13 11:52:18 -0400

    Update Landing.js
    </h3>

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
     return (
         <div className="landing-container">
             <InstagramLoop />
-            <h3> The MyNext4 team has decided to move on from our LLC; The sign-in, course, college, and career search features are all unusable as of December 12, 2023.
+            <h3> The MyNext4 team has decided to move on from our LLC; The sign-in, course, college, and career search features are all unusable as of December 12, 2023. </h3>
     
             {/* <LandingButton /> */}
         </div>
