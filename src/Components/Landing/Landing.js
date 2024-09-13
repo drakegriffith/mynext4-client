546abc96bf006f96f1f7ce921e9f88db357be723
Drake Griffith <Drakegriffith8@gmail.com>
2024-09-13 19:13:36 -0400

    Update Landing.js
    added extended blurb about shutdown of sign-in and search features

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -6,11 +6,18 @@ const Landing = () => {
     return (
         <div className="landing-container">
             <InstagramLoop />
-            <h3> The MyNext4 team has decided to move on from our LLC; The sign-in, course, college, and career search features are all unusable as of December 12, 2023. </h3>
+            <h3 style={{ 
+                textAlign: 'center', 
+                backgroundColor: 'white', 
+                borderRadius: '15px', 
+                padding: '20px', 
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' // Optional shadow for effect
+            }}>
+                The MyNext4 team has decided to move on from our LLC; The sign-in, course, college, and career search features are all unusable as of September 13, 2024.
+            </h3>
     
             {/* <LandingButton /> */}
         </div>
-    )
 }
 
 export default Landing;
