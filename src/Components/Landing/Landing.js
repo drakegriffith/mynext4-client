f28f5ac0a5b618781fc0cef4b82f29dce9912817
Drake Griffith <Drakegriffith8@gmail.com>
2024-09-14 19:16:48 -0400

    Update Landing.js

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -6,15 +6,7 @@ const Landing = () => {
     return (
         <div className="landing-container">
             <InstagramLoop />
-            <h3 style={{ 
-                textAlign: 'center', 
-                backgroundColor: 'white', 
-                borderRadius: '15px', 
-                padding: '20px', 
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' // Optional shadow for effect
-            }}>
-                The MyNext4 team has decided to move on from our LLC and has henceforth been dissolved. The sign-in, course, college, and career search features are no longer supported as of September 13, 2024.
-            </h3>
+            <h3> The MyNext4 team has decided to move on from our LLC and has henceforth been dissolved. The sign-in, course, college, and career search features are no longer supported as of September 13, 2024.</h3>
     
             {/* <LandingButton /> */}
         </div>
