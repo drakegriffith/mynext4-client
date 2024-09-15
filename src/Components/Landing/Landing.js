40ea7f076115ea4f6d40ca97263084bd80e4f898
Drake Griffith <Drakegriffith8@gmail.com>
2024-09-15 19:18:33 -0400

    Update Landing.js

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -5,7 +5,7 @@ import InstagramLoop from "./InstagramLoop";
 const Landing = () => {
     return (
         <div className="landing-container" styles="justify-content: center">
-            <h2> The MyNext4 team has decided to move on from our LLC and has henceforth been dissolved. The sign-in, course, college, and career search features are no longer supported as of September 13, 2024.</h2>
+            <p> The MyNext4 team has decided to move on from our LLC and has henceforth been dissolved. The sign-in, course, college, and career search features are no longer supported as of September 13, 2024.</p>
            
             {/* <LandingButton /> */}
         </div>
